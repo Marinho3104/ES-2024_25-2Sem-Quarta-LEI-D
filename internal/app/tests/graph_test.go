f3cc9db8f49dad705a44d6abc4314ea19a680d56
Misael Armando <misas.armando@gmail.com>
2025-04-09 09:46:16 +0100

    Some unit tests (not finished)

--- internal/app/tests/graph_test.go
+++ internal/app/tests/graph_test.go
@@ -7,11 +7,15 @@ import (
 
 func TestReadFile(t *testing.T) {
 	type testCase struct {
+		filename string
+		properties []Property
 
 		// Expected Values
 		expected string
 		err error
 	}
 
-	t.Run("Valid file read values", func(t *testing.T) {})
+	t.Run("Valid file read values", func(t *testing.T) {
+		tests := []testCase {}
+	})
 }
