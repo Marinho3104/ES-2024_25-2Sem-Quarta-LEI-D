728f37febe41ad888109fdd84ccba7f98d82fc67
Marinho <mariovieira2003@gmail.com>
2025-05-18 05:50:13 +0100

    First version working

--- cmd/ES-2024_25-2Sem-Quarta-LEI-D/main.go
+++ cmd/ES-2024_25-2Sem-Quarta-LEI-D/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	//"ES-2024_25-2Sem-Quarta-LEI-D/internal/app"
 	"ES-2024_25-2Sem-Quarta-LEI-D/internal/app"
+	// "fmt"
 	// "ES-2024_25-2Sem-Quarta-LEI-D/internal/handlers"
 	//"ES-2024_25-2Sem-Quarta-LEI-D/internal/handlers"
 	//"ES-2024_25-2Sem-Quarta-LEI-D/internal/versiondb"
@@ -11,10 +12,10 @@ import (
 // main initializes the program by setting up the required graphs and starting HTTP handlers.
 func main() {
 
-	app.Init_prices_by_concelho_and_fregusesia();
+	app.Init_all_suggestions_by_neighbour()
 
-	//versiondb.Main()
-	// app.GetGraph()
+	// app.Init_prices_by_concelho_and_fregusesia();
+
 	// app.GetOwnerGraph()
 	// handlers.Start()
 
